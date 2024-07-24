76fcd64348ac1fb20197dd58cb0c637ead1913c9
Meenakshy2002 <145977528+Meenakshy2002@users.noreply.github.com>
2024-07-24 11:19:33 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,157 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 15,
+   "id": "bc211446",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "[[ 9  6 64 79 96]\n",
+      " [24 33 44 63 59]\n",
+      " [92 55 83 66 12]]\n"
+     ]
+    }
+   ],
+   "source": [
+    "from numpy import random\n",
+    "x=random.randint(100,size=(3,5))\n",
+    "print(x)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "a806d887",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "92\n"
+     ]
+    }
+   ],
+   "source": [
+    "from numpy import random\n",
+    "x=random.randint(100)\n",
+    "print(x)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "2279ba2e",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "0.6057513439719308\n"
+     ]
+    }
+   ],
+   "source": [
+    "from numpy import random\n",
+    "x=random.rand()\n",
+    "print(x)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "id": "6bcee824",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "4\n"
+     ]
+    }
+   ],
+   "source": [
+    "from numpy import random\n",
+    "x=random.randint(3,5)\n",
+    "print(x)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 6,
+   "id": "2be3eb50",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "[0.81574343 0.68870888 0.13677205 0.75367509 0.64515182]\n"
+     ]
+    }
+   ],
+   "source": [
+    "from numpy import random\n",
+    "x=random.rand(5)\n",
+    "print(x)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 7,
+   "id": "c095591a",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "[[0.52350849 0.9349566  0.70338158 0.76834552 0.51017927]\n",
+      " [0.80170673 0.85946632 0.89220514 0.23266141 0.01989674]\n",
+      " [0.68119937 0.93332042 0.15312869 0.62217999 0.76452864]]\n"
+     ]
+    }
+   ],
+   "source": [
+    "from numpy import random\n",
+    "x=random.rand(3,5)\n",
+    "print(x)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "b7c9799f",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
